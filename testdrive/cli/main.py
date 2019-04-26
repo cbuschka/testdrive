0c8c64e09c5eb282427809a527c92491a4a6f17c
conni <cbuschka@gmail.com>
2019-04-26 17:43:49 +0200

    Added banner.

--- testdrive/cli/main.py
+++ testdrive/cli/main.py
@@ -2,17 +2,16 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import logging
 import sys
 
 from testdrive.console_output import console_output
 from testdrive.context import Context
 from testdrive.run_command import RunCommand
 
-log = logging.getLogger(__name__)
-
 
 def main():
+    console_output.print_banner()
+
     try:
         context = Context()
         command = RunCommand(context)
--- testdrive/console_output.py
+++ testdrive/console_output.py
@@ -6,10 +6,18 @@ import time
 
 
 class ConsoleOutput(object):
-    def print(self, s, *args):
+    def print_banner(self):
+        print(" _            _      _      _           \n" +
+              "| |_ ___  ___| |_ __| |_ __(_)_   _____ \n" +
+              "| __/ _ \\/ __| __/ _` | '__| \\ \\ / / _ \\\n" +
+              "| ||  __/\\__ \\ || (_| | |  | |\\ V /  __/\n" +
+              " \\__\\___||___/\\__\\__,_|_|  |_| \\_/ \\___|\n" +
+              "                                        ", flush=True)
+
+    def print(self, s, *args, trim=True):
         ts = time.time()
-        formattedTs = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        print((formattedTs + " " + s.strip()).format(*args), flush=True)
+        formattedTs = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        print((formattedTs + " " + s.strip() if trim else s).format(*args), flush=True)
 
 
 console_output = ConsoleOutput()
